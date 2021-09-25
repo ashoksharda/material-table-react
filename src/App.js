311c540f6e6a152ee02a8fc424fa12051e76d89b
ashoksharda <ashoksharda7@gmail.com>
2021-09-25 12:57:10 +0530

    initialise project

---
+++ src/App.js
@@ -0,0 +1,128 @@
+import React, { useState, useEffect } from "react";
+import MaterialTable from "material-table";
+import { forwardRef } from "react";
+import "./App.css";
+
+// these are to get table icons required for this material table --Start
+
+import AddBox from "@material-ui/icons/AddBox";
+import ArrowDownward from "@material-ui/icons/ArrowDownward";
+import Check from "@material-ui/icons/Check";
+import ChevronLeft from "@material-ui/icons/ChevronLeft";
+import ChevronRight from "@material-ui/icons/ChevronRight";
+import Clear from "@material-ui/icons/Clear";
+import DeleteOutline from "@material-ui/icons/DeleteOutline";
+import Edit from "@material-ui/icons/Edit";
+import FilterList from "@material-ui/icons/FilterList";
+import FirstPage from "@material-ui/icons/FirstPage";
+import LastPage from "@material-ui/icons/LastPage";
+import Remove from "@material-ui/icons/Remove";
+import SaveAlt from "@material-ui/icons/SaveAlt";
+import Search from "@material-ui/icons/Search";
+import ViewColumn from "@material-ui/icons/ViewColumn";
+
+const tableIcons = {
+  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+  DetailPanel: forwardRef((props, ref) => (
+    <ChevronRight {...props} ref={ref} />
+  )),
+  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  PreviousPage: forwardRef((props, ref) => (
+    <ChevronLeft {...props} ref={ref} />
+  )),
+  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />),
+};
+
+// table icons required for material table --End
+
+function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(async () => {
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const userData = await response.json();
+      setData(userData);
+      
+    } catch (error) {
+      console.log(error.message);
+    }
+  }, []);
+
+  const columns = [
+    { title: "ID", field: "id" },
+    { title: "Name", field: "name" },
+    { title: "Username", field: "username" },
+    { title: "Email", field: "email" },
+    { title: "Phone", field: "phone" },
+    { title: "Website", field: "website" },
+  ];
+
+  return (
+    <div className="App">
+      <h1>User Data Table</h1>
+      <MaterialTable
+        icons={tableIcons}
+        columns={columns}
+        data={data}
+        editable={{
+          onRowAdd: (newRow) =>
+            new Promise((resolve, reject) => {
+              //console.log(newRow);
+              setData([...data, newRow]);
+              setTimeout(() => {
+                resolve();
+              }, 500);
+            }),
+          onRowUpdate: (newRow, oldRow) =>
+            new Promise((resolve, reject) => {
+              const updatedData = [...data];
+              updatedData[oldRow.tableData.id] = newRow;
+              setData(updatedData);
+              // updatedData[oldRow.data.id] = newRow;
+              console.log(newRow, oldRow);
+              setTimeout(() => {
+                resolve();
+              }, 500);
+            }),
+
+          onRowDelete: (selectedRow) =>
+            new Promise((resolve, reject) => {
+              const updatedData = [...data];
+              updatedData.splice(selectedRow.tableData.id, 1);
+              setData(updatedData);
+              setTimeout(() => {
+                resolve();
+              }, 1000);
+            }),
+        }}
+        options={{
+          addRowPosition: "first",
+          actionsColumnIndex: -1,
+          
+        }}
+      />
+    </div>
+  );
+}
+
+export default App;
